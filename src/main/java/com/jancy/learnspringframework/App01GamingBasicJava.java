9a8ae6f01b2ce8976bbedab3b0d9b6ccfb828860
Jancy <s.jancyjerin@gmail.com>
2023-07-05 16:20:19 -0500

    Test change

--- src/main/java/com/jancy/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/jancy/learnspringframework/App01GamingBasicJava.java
@@ -11,7 +11,6 @@ public class App01GamingBasicJava {
 	public static void main(String[] args) {
 		
 		
-		  //var game=new MarioGame();
 		 //var game=new SuperContraGame();
 		PacmanGame game=new PacmanGame();    //1.object creation
 		GameRunner gameRunner=new GameRunner(game);   //2. Object creation + wiring of Dependencies .Game is adependency of GameRunner
